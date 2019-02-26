4a91d2fbe51718079add66f98cc9d1ebddaf4cd0
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2019-02-26 16:45:29 -0700

    ASP.NETStart

---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Hello World" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FirstPage.aspx.cs" Inherits="WebApp.SamplePages.FirstPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="yourname" runat="server" Text="Enter Your Name"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <asp:Literal ID="OutPutMessage" runat="server"></asp:Literal>
+    <asp:Button ID="PressMe" runat="server" Text="Press Me" OnClick="PressMe_Click" />
+     <br />
+</asp:Content>
---
+++ BasicASPX/WebApp/SamplePages/FirstPage.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class FirstPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          //this method will execute EACH and EVERY time this page is processed
+          //this method will execute before any event method
+        }
+
+        protected void PressMe_Click(object sender, EventArgs e)
+        {
+            OutPutMessage.Text = "Hello" + yourname.Text;
+        }
+    }
+}
